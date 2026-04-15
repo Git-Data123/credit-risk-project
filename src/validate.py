3a87ef0cab46ae69fdfef44108e0bfbd9f15815c
Git-Data123 <darlingtonekeopara@gmail.com>
2026-04-15 23:42:25 +0100

    Update validate.py

--- src/validate.py
+++ src/validate.py
@@ -2,13 +2,11 @@ import pandas as pd
 
 def validate_data(df):
     print("\n--- VALIDATION REPORT ---\n")
-
     print("Missing Values:")
     print(df.isnull().sum())
-
     print("\nDuplicate Rows:")
     print(df.duplicated().sum())
 
 if __name__ == "__main__":
-    df = pd.read_csv("SampleSuperstore.csv")
+    df = pd.read_csv("data/raw/SampleSuperstore.csv", encoding="latin1")
     validate_data(df)
